4f4bf93b7c350bb89eab65049910515c863efe22
kirkkao <86942183+kirkkao@users.noreply.github.com>
2021-07-06 12:50:24 +0800

    Add files via upload

---
+++ scholarship.py
@@ -0,0 +1,8 @@
+score = float(input('你好，我是校長。你的數學成績是?'))
+score2 = float(input('那你的英語成績是?'))
+if score == 100 or score2 == 100:
+    print('得到獎學金')
+elif score >=90 and score2>=90:
+    print('得到獎學金')
+else:
+    print ('受罰')
