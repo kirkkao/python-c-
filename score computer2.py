893b1af930ab0cf881c4d83f825f13c1619cffb0
kirkkao <86942183+kirkkao@users.noreply.github.com>
2021-07-08 10:13:08 +0800

    Add files via upload

--- score computer2.py
+++ score computer2.py
@@ -3,15 +3,16 @@ score2 = float(score)
 print(score)
 print(type(score))
 print(type(score2))
-if score2>100:
+if score2>=100:
     print('A+')
-elif score2<100 and score2>80:
+elif score2<100 and score2>=80:
     print('A')
-elif score2<80 and score2>60:
+elif score2<80 and score2>=60:
     print('B')
-elif score2<60 and score2>40:
+elif score2<60 and score2>=40:
     print('C')
-elif score2<40 and score2>20:
+elif score2<40 and score2>=20:
     print('D')
-elif score2<20 and score2>0:
+elif score2<20 and score2>=0:
     print('E')
+    
